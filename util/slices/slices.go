264c27abafe3173f60fa17ed8bae7b26ed8026b5
Martin Nyaga <martin@martinnyaga.com>
2022-12-06 08:10:59 +0000

    Add slices.Shift

--- util/slices/slices.go
+++ util/slices/slices.go
@@ -23,3 +23,14 @@ func PopN[T any](s *[]T, n int) ([]T, error) {
 	(*s) = (*s)[:size-(n)]
 	return val, nil
 }
+
+func Shift[T any](s *[]T) (T, error) {
+	size := len(*s)
+	if size == 0 {
+		var empty T
+		return empty, fmt.Errorf("Slice is empty")
+	}
+	val := (*s)[0]
+	(*s) = (*s)[1:]
+	return val, nil
+}
